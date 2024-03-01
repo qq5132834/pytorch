09aba135503177bdbfa99e7add24539e864588e5
qq5132834 <513283439@qq.com>
2024-03-01 14:37:41 +0800

    numpy

--- graph/graph/image.py
+++ graph/graph/image.py
@@ -3,11 +3,28 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy
 
+a1 = numpy.array([[1,2],
+                  [3,4],
+                  [5,6]])
+height, width = a1.shape
+a2 = numpy.zeros([3,2,2], dtype="int")
+
+for i in range(height):
+    for j in range(width):
+        v = a1[i][j]
+        for k in range(width):
+            a2[i][j][k] = v
+            print(a2[i][j][k])
+
+
+
+
 # 读
 xzImg = mpimg.imread('D:/AAAAAAAAAAAAAAAAAAAA/github/chrome_plugin_run_localProgram/chrome/src/test/resources/person.jpg')
-xzHeight, xzWidth, _ = xzImg.shape
+xzHeight, xzWidth, _a = xzImg.shape
 print("width:", xzWidth)
 print("height:", xzHeight)
+print("_a:", _a)
 print(type(xzImg))
 print(type(xzImg[0][0][0]))
 print(type(xzImg[0]))
@@ -15,19 +32,18 @@ print(type(xzImg[0]))
 print(len(xzImg))
 print(len(xzImg[0]))
 
-xzy = numpy.empty([xzWidth, xzHeight, xzWidth], dtype="int")
+xzy = numpy.empty([xzHeight, xzWidth, xzWidth], dtype="int")
 print(len(xzy))
 print(len(xzy[0]))
 print(len(xzy[0][0]))
 
 for z in range(xzHeight):
+    print(z)
     for x in range(xzWidth):
-        p = xzImg[x][z][0]
+        p = xzImg[z][x][0]
         for y in range(xzWidth):
-            v = xzy[x][z][y]
-            xzy[x][z][y]= p
-
-
+            xzy[z][x][y]=p
+            # print(xzy[x][z][y])
 
 
 # print(xzImg)
